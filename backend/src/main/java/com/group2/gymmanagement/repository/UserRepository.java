80ed6d8cef5b4b3f474c20179bd6e4bf5db1ec43
Ngọc Việt <VietDangg2005@gmail.com>
2025-11-24 19:59:53 +0700

    CRUD

--- backend/src/main/java/com/group2/gymmanagement/repository/UserRepository.java
+++ backend/src/main/java/com/group2/gymmanagement/repository/UserRepository.java
@@ -1,7 +1,23 @@
 package com.group2.gymmanagement.repository;
 
 import com.group2.gymmanagement.entities.User;
+import com.group2.gymmanagement.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+import java.util.Optional;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByUserName(String userName);
+
+  boolean existsByUserName(String userName);
+
+  List<User> findByRole(UserRole role);
+
+  Optional<User> findByEmail(String email);
+
+  boolean existsByEmail(String email);
 }
